3a4ae6fee31854b1cf8effd9f0e5ea51a0d0270c
Nethra <nethra12a@gmail.com>
2018-01-08 16:10:12 +0530

    Added message functionality

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessagesService } from './messages.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [HeroService],
+  providers: [HeroService, MessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,13 +3,15 @@ import { Hero } from './hero';
 import { Heroes } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessagesService } from './messages.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService : MessagesService) { }
 
   getHeroes() : Observable<Hero[]> {
+    this.messageService.add("HeroService: fetched heroes");
     return of(Heroes);
   }
 }
